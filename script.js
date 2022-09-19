82bf74342854b11dbed0647905024fd91db1f835
OlegRudniev <olegolegoleg1705@mail.ru>
2022-09-19 09:56:55 +0300

    HW-2

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-    };
-    const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)); document.querySelector('.goods-list').innerHTML = goodsList.join('');
-    };
-   document.addEventListener('DOMContentLoaded', () => {
-    renderGoodsList(goods); 
-   }); 
+class GoodsItem{
+  constructor(title='No name', price='No price'){
+    this.title = title;
+    this.price = price;
+  }
+  render(){
+    return`<div class="goods-item"><h3>${this.title}</h3><p>${this.price} рублей</p></div>`;
+  }
+}
+class GoodsList{
+  constructor(container = '.container'){
+    this.container = container;
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [{
+        title: 'Shirt',
+        price: 150
+      },
+      {
+        title: 'Socks',
+        price: 50
+      },
+      {
+        title: 'Jacket',
+        price: 350
+      },
+      {
+        title: 'Shoes',
+        price: 250
+      },
+      {
+        price: 150
+      }
+    ];
+  }
+  render(){
+    document.querySelector(this.container).innerHTML=this.goods.reduce((ass,item)=>{
+      const good = new GoodsItem(item.title, item.price);
+      return ass+=good.render();
+    }, '');
+  }
+  calcAllGoods(){
+    const totalPrice = document.querySelector('.goods-total span');
+    let sum = 0;
+    this.goods.forEach((elem)=>{
+      sum+= elem.price;
+    });
+    totalPrice.textContent = sum;
+  }
+  }
+class RemoveItem{}
+class AddItem{}
+class CheckOut{}
 
-
+const list = new GoodsList('.goods-list');
+list.fetchGoods();
+list.render();
+list.calcAllGoods();
